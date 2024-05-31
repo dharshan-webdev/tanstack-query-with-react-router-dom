f02af71219d38b598db7c1c53c867d2b698e589d
dharshan-webdev <dharshan.webdev@gmail.com>
2024-05-31 14:39:32 +0530

    Integrated Tanstack router with react-router

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { Navbar } from '../components/ui/navigation-menu'
+import { Navbar } from '../components/ui/NavigationMenu'
 
 export const Root = () => {
   return (
